ae9f6f353c8924002cc2989e4f6e194f8361e1e0
joelsoncrj <joelsonrocha.eng@gmail.com>
2019-09-18 22:55:49 -0300

    questao 1 - a funcionando corretamente

--- Pos-Graduacao Exercicios- Lista 1/01a.py
+++ Pos-Graduacao Exercicios- Lista 1/01a.py
@@ -11,42 +11,26 @@ import numpy as np
 
 """rocchio """
 
-"""
-função para separar as amostras de acordo com o label
-ou seja:
-data_label1
-data_label3
-data_label3
-"""
 
-def split_by_labels(data,label,labels_column):
-    index = data[data.'{}'.format(labels_column) == label].index
-    index_data_label = list(index)
-    
-    return index_data_label
 
-"""
-função para calcular os centroides das classes
-""" 
-
-def sample_centroids(data,sample_index_list,n_atributes):
-    m=np.empty((7),dtype=float)
-    for i in range (2,n_atributes):
-        m[i]=data.loc[sample_index_list,i].median()
+def sample_centroids(data):
+    m=np.empty((5),dtype=float)
+    for i in range (0,data.shape[1]-1):
+        m[i]=np.mean(data[:,i],dtype=np.float64)
     return m
 
 """
 Funcao para calcular distancia das amostras de teste ate os centroides de cada
 classe
 """
-def rocchio(data,Center_1,Center_2,Center_3,n_atributes):
+def rocchio(data,Center_1,Center_2,Center_3):
     labels=[]
     euclidian=np.empty((3),dtype = float)
-    for i in range(0,len(data)):
-        for j in range(2,n_atributes):
-            euclidian[0]=np.sqrt(np.power(np.sum(Center_1[j]-data.loc[i,'{}'.format(j)]),2))
-            euclidian[1]=np.sqrt(np.power(np.sum(Center_2[j]-data.loc[i,'{}'.format(j)]),2))
-            euclidian[2]=np.sqrt(np.power(np.sum(Center_3[j]-data.loc[i,'{}'.format(j)]),2))
+    for i in range(0,data.shape[0]):
+        for j in range(0,(data.shape[1]-1)):
+            euclidian[0]=np.sqrt(np.power(np.sum(Center_1[j]-data[i,j]),2))
+            euclidian[1]=np.sqrt(np.power(np.sum(Center_2[j]-data[i,j]),2))
+            euclidian[2]=np.sqrt(np.power(np.sum(Center_3[j]-data[i,j]),2))
             
         if(np.min(euclidian)==euclidian[0]):
             labels.append(1)
@@ -56,40 +40,29 @@ def rocchio(data,Center_1,Center_2,Center_3,n_atributes):
             labels.append(3)
     return labels
 
-def NN(data_training,data_test):
-    distances=[]
+def NN(trainning_array,test_array):
     labels=[]
-    for i in range(0,len(data_test)):
-        test=np.array(data_test.loc[i,0:6])
-        for z in range(0,len(data_training)):
-            trainning=np.array(data_training.loc[z,0:6])        
-            distances.append(np.sqrt(np.sum(np.power(np.subtract(trainning,test),2))))
-        
+    for i in range(0,test_array.shape[0]):
+        distances=np.zeros((123))
+        for z in range(0,trainning_array.shape[0]):
+            distances[z]=np.sqrt(np.sum(np.power(np.subtract(test_array[i,0:5],trainning_array[z,0:5]),2)))
         #min[0] = index of min value and min[1] is the min value
-        min = np.where(distances == np.amin(distances))
-        index = int(min[0])
-        labels.append(data_training.loc[index,7])
-        distances.clear()
-        
+        #print(distances)
+        distances = list(distances)
+        min_index = distances.index(min(distances))
+        #print(min_index)
+        #print(min_index)
+        labels.append(trainning_array[min_index,5])  
     return np.array(labels)
 
-##calculando acurácian_atributes
+#calculando acurácia
 
 def accuracy(labels,prediction):
     count=0
-    for i in range(0,len(labels_vector)):
+    for i in range(0,labels.shape[0]):
         if(labels[i]==prediction[i]):
             count=count+1
     return (count/len(labels))*100
-
-
-
-def remove_samples_by_value(data,n_atributes):
-    for i in range(0,len(data.index)):
-        if(data.loc[i,:]<-100):
-            print(i)
-    #print(data)                
-    return data
 #------------------------------------------------------------------
 """lendo os arquivos"""
 data_test = pd.read_csv('nebulosa_test.txt',sep=' ',header = None)
@@ -99,39 +72,61 @@ data_trainning = pd.read_csv('nebulosa_train.txt',sep=' ',header = None)
 header=['A', 'B' ,'C', 'D', 'E', 'F', 'F', 'LABEL']
 data_test.columns= header
 data_trainning.columns=header
-
+#removendo atributos que nao tem influencia na tarefa de classificacao
 data_test=data_test.drop(columns=['A','B'])
 data_trainning=data_trainning.drop(columns=['A', 'B'])
+#transformando amostras de teste em um numpy array
+samples_for_test = np.array(data_test)
+
+#pre processamento por intervalo de valor dos atributos
+samples_for_test=samples_for_test[(samples_for_test[:,0] >0) & (samples_for_test[:,0] <= 4.5)]
+samples_for_test=samples_for_test[(samples_for_test[:,1] >0) & (samples_for_test[:,1] <= 4.5)]
+samples_for_test=samples_for_test[(samples_for_test[:,2] >0) & (samples_for_test[:,2] <= 4.5)]
+samples_for_test=samples_for_test[(samples_for_test[:,3] >0) & (samples_for_test[:,3] <= 4.5)]
+samples_for_test=samples_for_test[(samples_for_test[:,4] >0) & (samples_for_test[:,4] <= 4.5)]
 
 
-#   data_trainning.drop(data_trainning[data_trainning[i] <= 0].index, inplace=True)
-
-
-print(len(data_trainning))
-#separando amostras por label
+#separando amostras de treinamento por label
 label1=data_trainning[data_trainning.LABEL == 1]
 label1=np.array(label1)
 label2=data_trainning[data_trainning.LABEL == 2]
 label2=np.array(label2)
 label3=data_trainning[data_trainning.LABEL == 3]
 label3=np.array(label3)
+#pre processamento por intervalo de valor dos atributos
+label1=label1[(label1[:,0] >0) & (label1[:,0] <= 4.5)]
+label1=label1[(label1[:,1] >0) & (label1[:,1] <= 4.5)]
+label1=label1[(label1[:,2] >0) & (label1[:,2] <= 4.5)]
+label1=label1[(label1[:,3] >0) & (label1[:,3] <= 4.5)]
+label1=label1[(label1[:,4] >0) & (label1[:,4] <= 4.5)]
 
-print(label1[label1[:,1] != -100])
+label2=label2[(label2[:,0] >0) & (label2[:,0] <= 4.5)]
+label2=label2[(label2[:,1] >0) & (label2[:,1] <= 4.5)]
+label2=label2[(label2[:,2] >0) & (label2[:,2] <= 4.5)]
+label2=label2[(label2[:,3] >0) & (label2[:,3] <= 4.5)]
+label2=label2[(label2[:,4] >0) & (label2[:,4] <= 4.5)]
+
+label3=label3[(label3[:,0] >0) & (label3[:,0] <= 4.5)]
+label3=label3[(label3[:,1] >0) & (label3[:,1] <= 4.5)]
+label3=label3[(label3[:,2] >0) & (label3[:,2] <= 4.5)]
+label3=label3[(label3[:,3] >0) & (label3[:,3] <= 4.5)]
+label3=label3[(label3[:,4] >0) & (label3[:,4] <= 4.5)]
+
 #calculando centroides para as classes
-C1=sample_centroids(data_trainning,index_label1,7)
-C2=sample_centroids(data_trainning,index_label2,7)
-C3=sample_centroids(data_trainning,index_label3,7)
-
+C1=sample_centroids(label1)
+C2=sample_centroids(label2)
+C3=sample_centroids(label3)
 
 #gerando novo  vetor de labels para amostras de teste 
-prediction_vector_rocchio= np.array(rocchio(data_test,C1,C2,C3,7))
-labels_vector=np.array(data_test.loc[:,7])
+prediction_vector_rocchio= rocchio(samples_for_test,C1,C2,C3)
+labels_vector=samples_for_test[:,5]
 
 accuracy_rocchio = accuracy(labels_vector,prediction_vector_rocchio)
    
 print("Accuracy using Rocchio: {0:.2f}%".format( accuracy_rocchio))
+trainning_vector = np.concatenate((label1,label2,label3))
 
-prediction_vector_NN=NN(data_trainning,data_test)
+prediction_vector_NN=NN(trainning_vector,samples_for_test)
 accuracy_NN=accuracy(labels_vector,prediction_vector_NN)
 print("Accuracy using NN: {0:.2f}%".format( accuracy_NN))
     
