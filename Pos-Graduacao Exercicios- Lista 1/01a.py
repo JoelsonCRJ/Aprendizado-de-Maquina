42ff50876bd00bc653b3b70bc60ca9720e2e6f9b
JoelsonCRJ <joelsonrocha.eng@gmail.com>
2019-09-24 17:00:39 -0300

    up

--- Pos-Graduacao Exercicios- Lista 1/01a.py
+++ Pos-Graduacao Exercicios- Lista 1/01a.py
@@ -8,13 +8,13 @@ Created on Thu Sep 12 09:26:56 2019
 
 import pandas as pd
 import numpy as np
-
+from scipy import stats
 """rocchio """
 
 
 
 def sample_centroids(data):
-    m=np.empty((5),dtype=float)
+    m=np.empty((data.shape[1] -1),dtype=float)
     for i in range (0,data.shape[1]-1):
         m[i]=np.mean(data[:,i],dtype=np.float64)
     return m
@@ -57,6 +57,7 @@ def NN(trainning_array,test_array):
 
 #calculando acurácia
 
+
 def accuracy(labels,prediction):
     count=0
     for i in range(0,labels.shape[0]):
@@ -79,9 +80,9 @@ data_trainning=data_trainning.drop(columns=['A', 'B'])
 samples_for_test = np.array(data_test)
 
 #pre processamento por intervalo de valor dos atributos
-samples_for_test=samples_for_test[(samples_for_test[:,0] >0) ]
-samples_for_test=samples_for_test[(samples_for_test[:,1] >0) ]
-samples_for_test=samples_for_test[(samples_for_test[:,2] >0) ]
+samples_for_test=samples_for_test[(samples_for_test[:,0] >0)]
+samples_for_test=samples_for_test[(samples_for_test[:,1] >0)]
+samples_for_test=samples_for_test[(samples_for_test[:,2] >0)]
 samples_for_test=samples_for_test[(samples_for_test[:,3] >0)]
 samples_for_test=samples_for_test[(samples_for_test[:,4] >0)]
 
@@ -122,11 +123,30 @@ prediction_vector_rocchio= rocchio(samples_for_test,C1,C2,C3)
 labels_vector=samples_for_test[:,5]
 
 accuracy_rocchio = accuracy(labels_vector,prediction_vector_rocchio)
-   
+print('Alternativa A - removendo apenas amostras com dados inconsistentes')
+
 print("Accuracy using Rocchio: {0:.2f}%".format( accuracy_rocchio))
 trainning_vector = np.concatenate((label1,label2,label3))
 #print(len(trainning_vector))
 prediction_vector_NN=NN(trainning_vector,samples_for_test)
 accuracy_NN=accuracy(labels_vector,prediction_vector_NN)
 print("Accuracy using NN: {0:.2f}%".format( accuracy_NN))
-    
+print('------------------------------------------------------------------')
+
+#pre-processamento de outliers
+#trocar valor de outlier por moda do atributo
+rounded_trainning_vector = np.around(trainning_vector, decimals=0) 
+rounded_test_vector = np.around(samples_for_test,decimals = 0)
+
+def change_to_mode(array):
+    for i in range(1,array.shape[0]-1):
+        for j in range(1,array.shape[1]):
+            if (array[i,j]<1 and array[i,j]>4):
+                array[i,j] = int(stats.mode(array[i,:]))
+    return array
+
+print(rounded_trainning_vector)
+rounded_test_vector = change_to_mode(rounded_test_vector)
+rounded_trainning_vector = change_to_mode(rounded_trainning_vector)
+print('-----------------------')
+print(rounded_trainning_vector)
