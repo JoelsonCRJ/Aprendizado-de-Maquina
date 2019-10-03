8fd52e7ee76a4a0851086565c67d1c26bfe62095
JoelsonCRJ <joelsonrocha.eng@gmail.com>
2019-10-03 11:20:33 -0300

    questao 2b

--- 01/02b.py
+++ 01/02b.py
@@ -8,18 +8,20 @@ data = data.drop(data.columns[0], axis=1)
 data = np.array(data)
 trainning_data = data[0:300,:]
 test_data = data[300:data.shape[0],:]
-print(trainning_data.shape[1])
+ 
 
 def get_x(array):
-    X=np.zeros((array.shape[1]-1,array.shape[0]),dtype = np.float32)
-    for i in range(0,array.shape[1]-1):
-        for j in range(0, array.shape[0]):
-            X[i][j]=(array[i][j] - np.mean(array[i,:],dtype=np.float64))/np.std(array[i,:],dtype=np.float32)
-            
+    
+    X=np.zeros((array.shape[0],array.shape[1]),dtype = np.float32)
+    for i in range(0,array.shape[0]):
+        for j in range(0, array.shape[1]):
+            X[i][j]=(array[i][j] - np.mean(array[:,j],dtype=np.float32))/np.std(array[:,j],dtype=np.float32)
+        
     return X
 
-print(trainning_data[:,1:trainning_data.shape[1]].shape[1])
-X_til = get_x(trainning_data[:,1:trainning_data.shape[1]])
-print(X_til)
-            
+
+X_til = get_x(trainning_data[:,2:trainning_data.shape[1]]) # atributo 1 ate o final 
+X_til_transposed = np.transpose(X_til)
+N=29 # tirando dois primeiros atributos pois são irrelevantes
+C=1/(N-1)* 
 #    np.linalg.eigh(X,UPLO = 'U')
