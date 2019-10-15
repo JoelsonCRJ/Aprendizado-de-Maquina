fd91af3ee6eba9bf8833285639d765644538f52a
JoelsonCRJ <joelsonrocha.eng@gmail.com>
2019-10-15 15:06:46 -0300

    questao 03 feita

--- 01/03.py
+++ 01/03.py
@@ -32,14 +32,21 @@ def get_tal(a1,a2,combinations):
             positivos = positivos +1
         else:
             negativos=negativos+1
-    print(positivos)
-    print(negativos)
-    
+   
     N=(a1.size*(a1.size-1))/2
-    print(N)
+
     tal = (positivos-negativos)/(N)
     return tal
 
+def get_covariance(data):
+    X_til=np.zeros((data.shape[0],data.shape[1]),dtype=np.float32)
+    for i in range(0,data.shape[0]):
+        for j in range(0,data.shape[1]):
+            X_til[i][j]=data[i][j]-np.mean(data[:,j],dtype=np.float32) 
+            
+    C=(1/(data.shape[0]-1))*(X_til.transpose().dot(X_til))
+    P=C[0][1]/(np.sqrt(C[0][0])*np.sqrt(C[1][1]))
+    return P
 #-----------------------
 
 
@@ -89,18 +96,40 @@ print('-----')
 #-------letra c ------
 
 combinations = np.array(list(combinations(np.arange(0,29),2)))
-print(combinations.shape[0])
+
 tal=get_tal(x,y,combinations)
 tal=abs(tal)
-print(tal)
-
+print('tal: {0:.2f}'.format(tal))
+N=x.size
 z_5p = 1.96
 z_1p = 2.33
 
-
-
-
+print('z1-a/2(5%) : {0:.2f}'.format(z_5p*np.sqrt(2*(2*N+5)/(9*N*(N-1)))))
+print('z1-a/2(1%) : {0:.2f}'.format(z_1p*np.sqrt(2*(2*N+5)/(9*N*(N-1)))))
+if(tal > z_5p*np.sqrt(2*(2*N+5)/(9*N*(N-1)))):
+    print('A hipótese nula foi rejeitada para 5% e existe a possibilidade de haver dependência entre x e y com 95% de significância')
+if(tal > z_1p*np.sqrt(2*(2*N+5)/(9*N*(N-1)))):
+    print('A hipótese nula foi rejeitada para 1% e existe a possibilidade de haver dependência entre x e y com 99% de significância')
             
+print('-----')
+
+#letra d
 
 
 
+P=get_covariance(data)
+print('P = {0:.2f}'.format(P))
+
+t_0=P*(np.sqrt(N-2))/(np.sqrt(1-np.power(P,2)))
+t_0 = abs(t_0)
+print('|t_0| = {0:.2f}'.format(t_0))            
+
+z_5p=2.052
+z_1p=2.771 
+
+if(t_0 > z_5p):
+    print('A hipótese nula foi rejeitada para 5%, portanto há confiabilidade de 95% ')
+if(t_0 > z_1p):
+    print('A hipótese nula foi rejeitada para 1%, portanto há confiabilidade de 99%  ')
+            
+print('-----')
