57ca0c9f3f1a1ce58f9c2958e29cdff5d797666f
joelsoncrj <joelsonrocha.eng@gmail.com>
2019-11-18 22:32:17 -0300

    questao 6 feita

--- 02/6.py
+++ 02/6.py
@@ -16,12 +16,11 @@ def sd(a):
 
 def noUm(ctreino,atreino):
     sdClasse=sd(ctreino)
-    I11 = np.where(atreino[:,0]<=2)
-    #print(I11)
+    I11 = np.array(np.where(atreino[:,0]<=2))
+    
     I12 = np.where(atreino[:,0]>2)
     a11 = atreino[I11,0]
     a12 = atreino[I12,0]
-#    print(a11)
     SDR1 = sdClasse - (a11.size/atreino.size)*sd(a11) - (a12.size/atreino.size)*sd(a12)
     
     
@@ -53,38 +52,42 @@ def noUm(ctreino,atreino):
 
 
 
-def noDois(ctreino,atreino):
-    
+def noDois(ctreino,atrain):
+#    print(atreino)
     sdClasse = sd(ctreino)
-    I11 = np.where(atreino[:,0]<=2)
-    #print(I11)
-    I12 = np.where(atreino[:,0]>2)
-    a11 = atreino[I11,0]
-    a12 = atreino[I12,0]
+    J11 = np.array(np.where(atrain[:,0]<=2))
+    
+    J12 = np.where(atrain[:,0]>2)
+    
+    a11 = atrain[J11,0]
+    a12 = atrain[J12,0]
 #    print(a11)
-    SDR1 = sdClasse - (a11.size/atreino.size)*sd(a11) - (a12.size/atreino.size)*sd(a12)
+    SDR1 = sdClasse - (a11.size/atrain.size)*sd(a11) - (a12.size/atrain.size)*sd(a12)
     
     
-    I21 = np.where(atreino[:,1]<=3)
-    I22 = np.where(atreino[:,1]>3)
-    a21 = atreino[I21,1]
-    a22 = atreino[I22,1]
+    J21 = np.where(atrain[:,1]<=3)
+    J22 = np.where(atrain[:,1]>3)
+    a21 = atrain[J21,1]
+    a22 = atrain[J22,1]
     
-    SDR2 = sdClasse - (a21.size/atreino.shape[0])*sd(a21) - (a22.size/atreino.shape[0])*sd(a22)
+    SDR2 = sdClasse - (a21.size/atrain.shape[0])*sd(a21) - (a22.size/atrain.shape[0])*sd(a22)
     
-    I41 = np.where(atreino[:,3]<=4)
-    I42 = np.where(atreino[:,3]>4)
-    a41 = atreino[I41,3]
-    a42 = atreino[I42,3]
-    
-    SDR4 = sdClasse - (a41.size/atreino.shape[0])*sd(a41) - (a42.size/atreino.shape[0])*sd(a42)
+    J41 = np.where(atrain[:,3]<=4)
+    J42 = np.where(atrain[:,3]>4)
+    a41 = atrain[J41,3]
+    a42 = atrain[J42,3]
+    SDR4 = sdClasse - (a41.size/atrain.shape[0])*sd(a41) - (a42.size/atrain.shape[0])*0 #a42=0
     
     SDRF = np.array([SDR1,SDR2,SDR4])
     
     SDRF_max = np.amax(SDRF)
     INDEX = np.where(SDRF_max == np.amax(SDRF_max))
     
-    return INDEX,I11,I12,I21,I22,I41,I42
+    return INDEX,J11,J12,J21,J22,J41,J42
+
+
+
+
 
 #------------------------------------------------------------
 data = pd.read_csv('servo.txt', sep=",", header=None)
@@ -117,13 +120,45 @@ cteste= classes[ctreino.shape[0]:classes.shape[0]+1]
 #Alternativa A 
 #primeiro nó
 index1,a31,a32 = noUm(ctreino,atreino)
-print(a31)
+a31=np.array(a31)
+a32=np.array(a32)
+natreinoMenor = np.squeeze(atreino[a31],axis=0)
+nctreinoMenor = np.squeeze(ctreino[a31],axis=0)
+natreinoMaior = np.squeeze(atreino[a32],axis=0)
+nctreinoMaior = np.squeeze(ctreino[a32],axis=0)
 #segundo nó
-natreinoMenor = atreino[a31,:           ]
-nctreinoMenor = ctreino[a31,:]
-natreinoMaior = atreino[a32,:]
-nctreinoMaior = ctreino[a32,:]
-
 index2Menor,_,_,_,_,i41,i42  = noDois(nctreinoMenor,natreinoMenor)
+index2Maior,_,_,i21,i22,_,_=noDois(nctreinoMaior,natreinoMaior)
 
+#árvore de regressão
+C1 = np.squeeze(np.where((atreino[:,2]<=5) & (atreino[:,3]<=4)),axis=0)
+media1=np.mean(np.take(ctreino,C1))
+C2 = np.where((atreino[:,2]<=5) & (atreino[:,3]<4))
+media2=np.mean(np.take(ctreino,C2))
+C3 = np.where((atreino[:,2]>5) & (atreino[:,3]<=3))
+media3=np.mean(np.take(ctreino,C3))
+C4 = np.where((atreino[:,2]>5) & (atreino[:,3]>4))
+media4=np.mean(np.take(ctreino,C4))
+
+
+#alternativa b
+rotulo=np.zeros((ateste.shape[0]))
+for i in range(0,ateste.shape[0]):
+    if (ateste[i,2]<=4):
+        if(ateste[i,3]<=4):
+            rotulo[i]=media1
+        elif(ateste[i,3]>4):
+            rotulo[i]=media2
+    elif(ateste[i,2]>4):
+        if(ateste[i,1]<=3):
+            rotulo[i]=media3
+        elif(ateste[i,1]>3):
+            rotulo[i]=media4
+
+
+MAPE = np.sum(abs(cteste-rotulo)/abs(cteste))/len(cteste)*100
+print(MAPE)
+
+RMSE = np.sqrt(np.sum(np.power(cteste-rotulo,2))/len(cteste))
+print(RMSE)
 
